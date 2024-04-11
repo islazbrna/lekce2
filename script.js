80713b91aca49af1cf49b083cec96cb7f10e7c26
KatkaS <katerina.skocovska@gmail.com>
2024-04-11 19:46:18 +0200

    prvni verze

---
+++ script.js
@@ -0,0 +1,6 @@
+document.body.innerHTML = "<p>Muj mesicni prijem je " + (7*320*21) + "</p>"
+document.body.innerHTML += "Na dani zaplatim  " +Math.floor((7*320*21)*0.4*0.15);
+
+document.body.innerHTML += "Delka filmu je " + Math.floor(223/60) + " hodin" + (223 % 60) + " minut";
+
+document.body.innerHTML += "katerina" + "." + "skocovska" + "@mujmail.com";
